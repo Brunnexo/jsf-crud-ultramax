6170b645a0ee257c140636f18430f5353d07df84
Brunnexo <blimapcosta@gmail.com>
2021-04-13 11:39:31 -0300

    Início da página de alterações

--- src/main/java/br/com/ultramax/exemplojsf/web/CadastroBean.java
+++ src/main/java/br/com/ultramax/exemplojsf/web/CadastroBean.java
@@ -1,5 +1,36 @@
 package br.com.ultramax.exemplojsf.web;
 
-public class CadastroBean {
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.faces.bean.ManagedBean;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import br.com.ultramax.exemplojsf.main.PersistenceJPAConfig;
+
+@ManagedBean(name = "cadastroBean")
+public class CadastroBean {
+	protected AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
+	
+	
+	@PostConstruct
+	public void getSpringContext() {
+		ctx.register(PersistenceJPAConfig.class);
+		ctx.refresh();
+	}
+
+	@PreDestroy
+	public void closeSpringContext() {
+		ctx.close();
+	}
+	
+	public void listarCadastros() {
+		
+		
+		
+		
+		
+		
+		
+	}
 }
